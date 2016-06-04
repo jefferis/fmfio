b0bc92ad42c4bca465d7508ad2f784e581f05504
Gregory Jefferis <jefferis@gmail.com>
2016-06-04 20:54:40 +0100

    optionally return foreground bounding boxes

--- R/read.ufmf.frame.R
+++ R/read.ufmf.frame.R
@@ -2,11 +2,16 @@
 #'
 #' @details When reading multiple frames, the recommended idiom is to read the
 #'   header and store it in an object, then pass that object to read.ufmf along
-#'   with the integer index of the required frame.
+#'   with the integer index of the required frame. See \bold{examples}.
+#'
+#'   When present, bounding boxes for foreground pixels are returned in an N x 4
+#'   array where the four columns specify (x, y, width , height).
 #'
 #' @param x Either a path to a file on disk, a \code{\link{connection}} or a
 #'   parsed ufmf header object (as returned by \code{\link{read.ufmf.header}})
 #' @param framei Integer index of the frame to read
+#' @param return.boxes Whether to return an matrix describing the position of
+#'   pixels that differ from the mean image (see details, default \code{FALSE}).
 #' @return An array containing the image data with the frame timestamp as an
 #'   attribute.
 #' @seealso \code{\link{connection}}, \code{\link{read.ufmf.header}}
@@ -17,7 +22,7 @@
 #' frames=lapply(1:5, function(i) read.ufmf(h, i))
 #' }
 #'
-read.ufmf <- function(x, framei=NULL){
+read.ufmf <- function(x, framei=NULL, return.boxes=FALSE){
   if(is.character(x))
     x=file(x, open='rb')
 
@@ -108,6 +113,8 @@ read.ufmf <- function(x, framei=NULL){
   }
   im = aperm(im, c(3, 2, 1))
   attr(im, 'timestamp')=timestamp
+  if(return.boxes)
+    attr(im, 'boxes')=bb
   im
 }
 
