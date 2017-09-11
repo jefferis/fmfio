3f15687285e663fbbaa337a96d02e9eb4e1e7f62
Gregory Jefferis <jefferis@gmail.com>
2017-09-11 19:42:18 +0100

    Add package help

---
+++ R/fmfio-package.R
@@ -0,0 +1,16 @@
+#' Read Fly Movie Format image time series
+#'
+#' @details There are two FMF formats: \itemize{
+#'
+#' \item{\bold{fmf}}{ A very simple uncompressed movie format.}
+#'
+#' \item{\bold{ufmf}}{ Compressed format based on background subtraction that can
+#' provide very significant space savings.}
+#'
+#' }
+#' @name fmfio-package
+#' @aliases fmfio
+#' @seealso \code{\link{read.fmf}}, \code{\link{read.ufmf}}
+#' @docType package
+#' @keywords package
+NULL
